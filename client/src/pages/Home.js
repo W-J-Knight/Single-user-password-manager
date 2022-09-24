9f0a4b904b75e9aea4107de46223fa2a687736b5
William Jeffery Knight <William.Jeffery.Knight@protonmail.com>
2022-09-24 19:23:02 -0400

    made navbar home setup routes

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+function Home() {
+    return (
+        <Container>
+            <Row>
+                <Col md={8}>
+                    <h1>Single Use Password Manager</h1>
+                </Col>
+            </Row>
+        </Container>
+
+    )
+}
+
+export default Home
