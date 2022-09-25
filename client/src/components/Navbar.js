972fd9357e9c9499712e97e0c27b2ab0deaf2e07
William <William.Jeffery.Knight@protonmail.com>
2022-09-25 12:25:59 -0400

    made navbar responsive

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,31 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Link, useMatch, useResolvedPath, NavLink} from "react-router-dom";
+import { 
+    Navbar, 
+    NavbarBrand,
+    Collapse,
+    NavbarToggler,
+    Nav,
+    NavItem 
+} from "reactstrap";
+import { useState } from "react";
 
-export default function Navbar() {
+export default function NewNavbar() {
+    const [menuOpen, setMenuOpen ] = useState(false)
     return (
-        <nav className="nav">
+        <Navbar dark className="nav justify-content-md-evenly" expand='md' sticky='top' >
             <Link to="/" className="site-title">
                 Password Manager
             </Link>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+            <Collapse isOpen={menuOpen} navbar>
+            <div className='ms-auto' navbar sm={3}>
             <ul>
                 <CustomLink to="/add">Add</CustomLink>
                 <CustomLink to="/get">Retrieve</CustomLink>
             </ul>
-        </nav>
+            </div>
+            </Collapse>
+        </Navbar>
     )
 }
 
